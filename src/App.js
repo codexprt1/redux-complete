a4e6ff87bc893dd63134e009f99cf2da8bb6ba80
codexprt1 <codexprt1@gmail.com>
2021-09-16 17:42:36 +0530

    basic app structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import Counter from "./components/Counter";
 
 const App = () => {
 	return (
 		<div className='App'>
-			<h1>Redux Practice</h1>
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					{/* <Counter /> */}
+				</PersistGate>
+			</Provider>
 		</div>
 	);
 };
